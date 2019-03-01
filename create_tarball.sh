d384918fb009b52a1ebb42b3cf6e5f3c56068dc1
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2019-03-01 16:32:14 +0000

    make it work with git

--- create_tarball.sh
+++ create_tarball.sh
@@ -3,6 +3,6 @@
 # create a tar ball of all the files that are under subversion control
 
 # produce a list of files to include
-for i in $(svn status -v | grep -v create_tarball.sh | awk '{if (NF>3) print $NF}'); do if [ -f $i ]; then echo $i; fi; done > include_files
+for i in $(git ls-tree -r master --name-only | egrep -v 'create_tarball.sh|.gitignore'); do if [ -f $i ]; then echo $i; fi; done > include_files
 
 tar -cvzf thesis.tar.gz -T include_files
